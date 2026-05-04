36dbc9fe6f04fbe78fb3e23d957e2fbb4db4a311
JoshuaT <jttsat170@gmail.com>
2026-05-04 14:36:46 -0400

    setting up the project

---
+++ backend/src/main/java/com/money/MoneyTracker.java
@@ -0,0 +1,13 @@
+package com.money;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class MoneyTracker {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyTracker.class, args);
+    }
+
+}
